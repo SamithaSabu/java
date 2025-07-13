62bf6cdbbb6d60087185354428f7dfe8712d626b
SamithaSabu <samithasabu4857@gmail.com>
2025-07-13 19:31:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-public class HELLO
+public class HELLO
   {
-    public static void main(String args[])
+    public static void main(String args[])
     {
-      System.out.println("Hello Java");
+      System.out.println("Hello Java");
     }
   }
