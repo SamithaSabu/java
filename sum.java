dbc52a9d6d182e69fe3802efcb4dcffb3a848faf
SamithaSabu <samithasabu4857@gmail.com>
2025-07-13 20:19:59 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum
+  {
+    public static void main(String args[]) {
+      /** Author:Samitha Sabu
+        * Date :13/07/2025 */
+      int a=20;
+      int b=80;
+      System.out.println("Sum of numbers "+ a+" and "+ b +" is :"+(a+b));
+    }
+  }
