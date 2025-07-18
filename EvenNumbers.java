9d4df792c73a426c432c3d368b34f2026a24e551
SamithaSabu <samithasabu4857@gmail.com>
2025-07-18 21:59:51 +0530

    Create EvenNumbers.java

---
+++ EvenNumbers.java
@@ -0,0 +1,17 @@
+package module1;
+/** Author: SAMITHA SABU
+ * Date: 18/07/2025
+ */
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		int i;
+		System.out.println("Even numbers between 1 to 100 are :");
+		for(i=2;i<=100;i+=2) {
+			System.out.println(i);
+		}
+
+	}
+
+}
